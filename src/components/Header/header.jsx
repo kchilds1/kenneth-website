40fd2760164e31bbe9fe6327fa692fbc47baeae0
kchilds1 <kenneth.childs20@gmail.com>
2023-11-02 11:30:55 -0500

    changed background image

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,22 +1,143 @@
+// import Head from "next/head";
+// import React from "react";
+// import styles from "@/styles/Home.module.css";
+// import { useRouter } from "next/router";
+// import { Toolbar, Typography, Button, CardMedia } from "@mui/material";
+// export default function Header() {
+//   const router = useRouter();
+//   return (
+//     <>
+//       <Toolbar sx={{ padding: "3px" }}>
+//         <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+//           <CardMedia
+//             component="img"
+//             image="developer_logo.jpeg"
+//             style={{
+//               borderRadius: "100%",
+//               width: "100px",
+//               maxWidth: "100%",
+//               height: "auto",
+//             }}
+//             alt="logo"
+//           />
+//         </Typography>
+//         <a href="/">
+//           <Button
+//              sx={{
+          
+//                "@media (max-width:375px)": {
+//                  width:"4%",
+//                  fontSize:"10px",
+//                  padding:"5px",
+//                },
+//                marginRight: "2px",
+//                background: "linear-gradient(170deg, deepskyblue, navy 80%)",
+//                color: "white",
+//                borderRadius: "15px",
+//                padding: "5px",
+//              }}
+//           >
+//             HOME
+//           </Button>
+//         </a>
+//         <a href="/about">
+//           <Button
+//             sx={{
+//               "@media (max-width:375px)": {
+//                 width:"4%",
+//                 fontSize:"10px",
+//                 padding:"5px",
+                
+//               },
+//               marginRight: "2px",
+//               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
+//               color: "white",
+//               borderRadius: "15px",
+//               padding: "5px",
+//             }}
+//           >
+//             ABOUT
+//           </Button>
+//         </a>
+//         <a href="/projects">
+//           <Button
+//             sx={{
+//               // "@media (max-width:375px)": {
+//               //   width:"4%",
+//               //   fontSize:"10px",
+//               //   padding:"5px",
+//               // },
+//               marginRight: "2px",
+//               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
+//               color: "white",
+//               borderRadius: "15px",
+//               padding: "5px",
+//             }}
+//           >
+//             PROJECTS
+//           </Button>
+//         </a>
+//         <a href="/skills">
+//           <Button
+//             sx={{
+//               // "@media (max-width:375px)": {
+//               //   width:"4%",
+//               //   fontSize:"10px",
+//               //   padding:"5px",
+//               //  },
+//               marginRight: "2px",
+//               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
+//               color: "white",
+//               borderRadius: "15px",
+//               padding: "5px",
+//             }}
+//           >
+//             SKILLS
+//           </Button>
+//         </a>
+//         <a href="/contacts">
+//           <Button
+//             sx={{
+//               // "@media (max-width:375px)": {
+//               //   width:"4%",
+//               //   fontSize:"10px",
+//               //   padding:"5px",
+//               // },
+//               marginRight: "2px",
+//               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
+//               color: "white",
+//               borderRadius: "15px",
+//               padding: "5px",
+//             }}
+//           >
+//             CONTACT
+//           </Button>
+//         </a>
+//       </Toolbar>
+//     </>
+//   );
+// }
+ 
 import Head from "next/head";
 import React from "react";
 import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
 import { Toolbar, Typography, Button, CardMedia } from "@mui/material";
+
 export default function Header() {
   const router = useRouter();
+
   return (
     <>
-      <Toolbar sx={{ padding: "3px" }}>
+      <Toolbar sx={{ padding: "3px", position:"sticky", top:"0" }}>
         <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
           <CardMedia
             component="img"
             image="developer_logo.jpeg"
             style={{
               borderRadius: "100%",
-              width: "100px",
+               width: "5.5556rem",
               maxWidth: "100%",
-              height: "auto",
             }}
             alt="logo"
           />
@@ -25,16 +146,15 @@ export default function Header() {
           <Button
             sx={{
               
-              "@media (max-width:375px)": {
-                width:"4%",
-                fontSize:"10px",
-                padding:"5px",
-              },
+              width: "4em", 
+              fontSize: ".65em", 
+              padding: "0.2778em", 
               marginRight: "2px",
               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
               color: "white",
-              borderRadius: "15px",
-              padding: "5px",
+              borderRadius: "0.8333em", 
+              padding: "0.4778em", 
+              marginRight: "2px",
             }}
           >
             HOME
@@ -43,17 +163,15 @@ export default function Header() {
         <a href="/about">
           <Button
             sx={{
-              "@media (max-width:375px)": {
-                width:"4%",
-                fontSize:"10px",
-                padding:"5px",
-                
-              },
+              width: "4em",
+              fontSize: ".65em",
+              padding: "0.2778em",
               marginRight: "2px",
               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
               color: "white",
-              borderRadius: "15px",
-              padding: "5px",
+              borderRadius: "0.8333em",
+              padding: "0.4778em",
+              marginRight: "2px",
             }}
           >
             ABOUT
@@ -62,16 +180,14 @@ export default function Header() {
         <a href="/projects">
           <Button
             sx={{
-              "@media (max-width:375px)": {
-                width:"4%",
-                fontSize:"10px",
-                padding:"5px",
-              },
+              width: "4em",
+              fontSize: ".65em",
               marginRight: "2px",
               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
               color: "white",
-              borderRadius: "15px",
-              padding: "5px",
+              borderRadius: "0.8333em",
+              padding: "0.4778em",
+              marginRight: "2px",
             }}
           >
             PROJECTS
@@ -80,16 +196,14 @@ export default function Header() {
         <a href="/skills">
           <Button
             sx={{
-              "@media (max-width:375px)": {
-                width:"4%",
-                fontSize:"10px",
-                padding:"5px",
-              },
+              width: "4em",
+              fontSize: ".65em",
               marginRight: "2px",
               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
               color: "white",
-              borderRadius: "15px",
-              padding: "5px",
+              borderRadius: "0.8333em",
+              padding: "0.4778em",
+              marginRight: "2px",
             }}
           >
             SKILLS
@@ -98,16 +212,14 @@ export default function Header() {
         <a href="/contacts">
           <Button
             sx={{
-              "@media (max-width:375px)": {
-                width:"4%",
-                fontSize:"10px",
-                padding:"5px",
-              },
+              width: "4em",
+              fontSize: ".65em",
               marginRight: "2px",
               background: "linear-gradient(170deg, deepskyblue, navy 80%)",
               color: "white",
-              borderRadius: "15px",
-              padding: "5px",
+              borderRadius: "0.8333em",
+              padding: "0.4778em",
+              marginRight: "2px",
             }}
           >
             CONTACT
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,20 +1,18 @@
- import Head from "next/head";
- import { React, useState } from "react";
- import styles from "../styles/Home.module.css";
- import { useRouter } from "next/router";
- import Layout from "@/components/Layout/layout";
- import {
-   TextField,
-   Typography,
-   Box,
-   Button,
- } from "@mui/material";
+import Head from "next/head";
+import { React, useState } from "react";
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import Layout from "@/components/Layout/layout";
+import {
+  TextField,
+  Typography,
+  Box,
+  Button,
+} from "@mui/material";
 
-
- export default function Contacts() {
-
+export default function Contacts() {
   const router = useRouter();
-  
+
   const [contactName, setContactName] = useState("");
   const [company, setCompany] = useState("");
   const [email, setEmail] = useState("");
@@ -23,154 +21,144 @@
   const [isButtonDisabled, setButtonDisabled] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
-  async function handleSubmit(event){
-     event.preventDefault();
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-     if (!contactName || !email) {
+    if (!contactName || !email) {
       alert("Please fill in required fields.");
       return;
     }
 
     setButtonDisabled(true);
 
-  const infoPayload = {
-    company,
-    contactName,
-    email,
-    phoneNumber,
+    const infoPayload = {
+      company,
+      contactName,
+      email,
+      phoneNumber,
+    };
+
+    console.log("Payload: ", infoPayload);
+
+    try {
+      const response = await fetch("/api/contacts", {
+        method: "POST",
+        body: JSON.stringify(infoPayload),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        console.log("Data sent successfully!");
+        setSubmitted(true);
+      } else {
+        console.log("Failed to send data to the server.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    } finally {
+      setButtonDisabled(false);
+    }
+  }
+
+  const renderFormOrThankYou = () => {
+    if (submitted) {
+      return (
+        <Typography variant="h5" sx={{ textAlign: "center", color: "white" }}>
+          Thank you for your submission!
+        </Typography>
+      );
+    } else {
+      return (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            sx={{
+              background: "linear-gradient(170deg, deepskyblue, navy 80%)",
+              color: "white",
+              borderRadius: "1.3889rem", 
+              fontSize: "1.1111rem", 
+              fontWeight: "bold",
+              paddingBottom: "0.2778rem", 
+              marginBottom: "0.5556rem",  
+            }}
+            variant="contained"
+            onClick={handleSubmit}
+            disabled={isButtonDisabled}
+          >
+            SUBMIT
+          </Button>
+        </Box>
+      );
+    }
   };
 
-  console.log("Payload: ", infoPayload);
-  // SEND DATA OVER THE SERVER
-  try {
-    const response = await fetch("/api/contacts", {
-      method: "POST",
-      body: JSON.stringify(infoPayload),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      console.log("Data sent successfully!");
-      // Set submitted to true to display the thank you message
-      setSubmitted(true);
-    } else {
-      console.log("Failed to send data to the server.");
-    }
-  } catch (error) {
-    console.error("An error occurred:", error);
-  } finally {
-    // Enable the button after the form submission is complete
-    setButtonDisabled(false);
-  }
-};
-
-// Conditionally render the thank you message
-const renderFormOrThankYou = () => {
-  if (submitted) {
-    return (
-      <Typography variant="h5" sx={{ textAlign: "center", color: "white" }}>
-        Thank you for your submission!
-      </Typography>
-    );
-  } else {
-    return (
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Button
-          sx={{
-            "@media (max-width:375px)": {
-              width:"4%",
-              fontSize:"10px",
-              padding:"5px",
-            },
-            color: "white",
-            background: "linear-gradient(170deg, deepskyblue, navy 80%)",
-            margin: "4px",
-          }}
-          variant="contained"
-          onClick={handleSubmit}
-          disabled={isButtonDisabled}
-        >
-          SUBMIT
-        </Button>
-      </Box>
-    );
-  }
-};
-
-
-   return (
-     <Layout>
-       <Head>
+  return (
+    <Layout>
+      <Head>
         <title>Kenneth website contacts</title>
         <meta name="contacts page" content="contacts" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-       <main sx={{marginBottom:"20px"}}>
-          <Typography
-            variant="h3"
-            sx={{ display: "flex", justifyContent: "center" ,color:"white" }}
-          >
-            CONTACT
-          </Typography >
-          <Typography sx={{ display: "flex", justifyContent: "center" ,color:"white" }}>
+      <main> 
+        <Typography
+          variant="h3"
+          sx={{ display: "flex", justifyContent: "center", color: "white" }}
+        >
+          CONTACT
+        </Typography>
+        <Typography sx={{ display: "flex", justifyContent: "center", color: "white" }}>
           Question, Suggestion, or Complaint? Let Me Know.
-          </Typography>
-          {submitted ? ( // Render thank you message if submitted is true
+        </Typography>
+        {submitted ? (
           renderFormOrThankYou()
         ) : (
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                gap:"10px",
-              }}
-            >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              gap: "0.3556rem", 
+            }}
+          >
+            <TextField
+              label="Your Name"
+              value={contactName}
+              onChange={(e) => setContactName(e.target.value)}
+              required={true}
+              className={styles.input}
+            />
+            <TextField
+              label="Company Name"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+              className={styles.input}
+            />
+            <TextField
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required={true}
+              className={styles.input}
+            />
+            <TextField
+              label="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className={styles.input}
+            />
+            <Box>
               <TextField
-                label="Your Name"
-                value={contactName}
-                onChange={(e) => setContactName(e.target.value)}
-                required={true}
-                className={styles.input}
-               
-              />
-              <TextField
-                label="Company Name"
-                value={company}
-                onChange={(e) => setCompany(e.target.value)}
-                className={styles.input}
-              />
-              
-              <TextField
-                label="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required={true}
-                className={styles.input}
-                
-              />
-              
-              <TextField
-                label="Phone Number"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                className={styles.input}
-              />
-              
-              <Box >
-                <TextField
                 fullWidth
-                  label="Comments/Questions"
-                  multiline
-                  rows={3}
-                  columns={6}
-                  value={question}
-                  onChange={(e) => setQuestion(e.target.value)}
-                  className={styles.input}
-                />
-              </Box>
+                label="Comments/Questions"
+                multiline
+                rows={3}
+                columns={6}
+                value={question}
+                onChange={(e) => setQuestion(e.target.value)}
+                className={styles.input}
+              />
+            </Box>
             {renderFormOrThankYou()}
           </Box>
         )}
@@ -178,6 +166,7 @@ const renderFormOrThankYou = () => {
     </Layout>
   );
 }
+
 
 
 

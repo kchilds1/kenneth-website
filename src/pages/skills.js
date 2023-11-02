e8f7f8b372d4817fc4f44166cef5cc07e505e2a6
kchilds1 <kenneth.childs20@gmail.com>
2023-11-02 12:32:50 -0500

    added media query for box around card at 803px

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -25,17 +25,13 @@
        </Box>
        <Box
          sx={{ 
-          "@media (max-width:390px)": {
+          "@media (max-width:803px)": {
             marginTop:"25px",
           },
           display: "flex", justifyContent: "center", alignItems: "center", flexWrap:"wrap" }}
        >
          <Card sx={{ 
-          "@media (max-width:375px)": {
-            marginTop:"10%",
-            paddingRight:"20%",
-            paddingLeft:"20%",
-          },
+          
          
           maxWidth: 345 }}>
            <CardContent
